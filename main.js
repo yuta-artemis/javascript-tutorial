ebf596f37c89d48c50e5b082d1430afd62668b5f
yuta <yuta.nakatsuka.1992@gmail.com>
2022-08-05 11:29:59 +0900

    履歴機能とモーダルによる拡大表示を追加

--- main.js
+++ main.js
@@ -1,37 +1,81 @@
 (() => {
     let UnsplashAPI = "https://source.unsplash.com/featured/?";
 
-    let button = document.getElementById("search_button");
-    let input = document.getElementById("search_word");
+    let button = document.querySelector(".search_button");
+    let input = document.querySelector(".search_word");
 
-    let draw_area = document.getElementById("draw_area");
+    let history_area = document.querySelector('.history_area');
+
+    let draw_area = document.querySelector(".draw_area");
+
+    let modal_wrapper = document.querySelector('.modal_wrapper');
+    modal_wrapper.addEventListener('click', () => {
+        modal_wrapper.classList.toggle('show');
+        modal_wrapper.querySelector('img').remove();
+    });
 
     button.addEventListener("click", () => {
+        // inputから検索語句を取得
+        // 空白で分割し配列にしたあと、空の検索語句をfilterで削除
+        // 検索語句が何もなければ処理を終える
+        // 検索語句があればAPIのbaseURLに加え、表示用のURLを生成する
         let word = input.value.split(" ");
+        input.value = "";
         word = word.filter(w => {
             if (w != "") {
                 return w;
             }
         });
-
         if (word.length == 0 ) {
             return ;
         }
+        word = word.join(",");
+        let url = UnsplashAPI + word;
 
-        let url = UnsplashAPI + word.join(",");
+        let history = document.createElement('span');
+        history.textContent = word;
+        history.addEventListener('click', event => {
+            event.preventDefault();
+            let historys = document.querySelectorAll('.history_area span');
+            hisotrys = Array.from( historys );
+            let index = hisotrys.indexOf(event.target);
+            let active = document.querySelector('.active');
+            if (active) {
+                active.classList.remove('active');
+            }
+            let page = document.querySelectorAll('.page')[index];
+            if (page) {
+                page.classList.add('active');
+            }
+        });
+        history_area.appendChild(history);
 
-        let div = document.createElement("div");
-        div.classList.add("row");
+        // unsplashで取得した画像50枚を表示するpageを作成
+        // .activeがついているpageを探し、.activeを削除
+        // 新しく作成したpageに.activeを付与
+        let active_page = document.querySelector('.active');
+        if (active_page) {
+            active_page.classList.remove('active');
+        }
+        let page = document.createElement('div');
+        page.classList.add('page', 'active');
 
-        for (let i = 0; i < 5; i++) {
+
+        // 50枚画像を生成し、pageに追加する
+        for (let i = 0; i < 50; i++) {
             let img = new Image();
             img.setAttribute("src", url + "/" + Math.floor(Math.random() * (new Date()).getTime()) );
-            img.width = Math.floor(window.innerWidth / 5);
-            img.height = Math.floor(window.innerWidth / 5);
-            div.appendChild(img);
+            img.addEventListener('click', (event) => {
+                let modal_wrapper = document.querySelector('.modal_wrapper');
+                let modal = document.querySelector('.modal');
+                modal.appendChild(event.target.cloneNode());
+                modal_wrapper.classList.toggle('show');
+            });
+            page.appendChild(img);
         }
 
-        draw_area.insertBefore(div, draw_area.firstElementChild);
-        input.value = "";
+        // pageを表示領域に追加
+        draw_area.appendChild(page);
+        
     });
 })();
